0a3800b2982d078377d52a1244c503f3d8eede32
joharaAl <160887276+joharaAl@users.noreply.github.com>
2025-10-28 20:20:05 +0300

    models

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    #image   
+    
+    def __str__(self):
+        return self.name
+
+class BasketItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='basket_items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    def line_total(self):
+        return self.product.price * self.quantity
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'Order #{self.id} by {self.user.username}'
+
+
